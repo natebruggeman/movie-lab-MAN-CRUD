a03c19ff63585ddfb14d63718b361aaacca32bc0
Nate Bruggeman <you@example.com>
2019-10-18 15:14:42 -0500

    mongoose connected

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const connectionString = 'mongodb://localhost/new';
 
+mongoose.connect(connectionString, { useNewUrlParser: true,
+                                     useUnifiedTopology: true,
+                                     useCreateIndex: true,
+                                     useFindAndModify: false
+                                    });
+
+//connection test
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${connectionString}`);
+});
 
 
 
